6f1617a8f9c82d478691d1fc85ed27db6a5ebb7f
lin_ting_yu <rr04030403@gmail.com>
2025-10-03 16:12:21 +0800

    20251003

---
+++ 資料庫程式設計(劉)/20251003/ch2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $score = 61;
+        if($score>60){
+            echo "及格";
+        }else{
+            echo "不及格";
+        }
+    ?>
+
+
+</body>
+</html>
---
+++ 資料庫程式設計(劉)/20251003/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>php</h1>
+    <p><a href="ch1.php">第一章</a><br></p>
+    <a href="ch2.php">第二章</a>
+</body>
+</html>
